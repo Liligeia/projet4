8ecf7fc9aea5a80d12795d9dca47e16f4bdb59f1
Melanie <melanie.xelm@gmail.com>
2019-06-24 21:06:08 +0200

    Ajout Vue.php commentaires

--- Framework/Vue.php
+++ Framework/Vue.php
@@ -1,36 +1,61 @@
 <?php
-class Vue {
-    // Nom du fichier associé à la vue
+require_once 'Configuration.php';
+/**
+ * Classe modélisant une vue.
+ *
+ * @author Baptiste Pesquet
+ */
+class Vue
+{
+    /** Nom du fichier associé à la vue */
     private $fichier;
-    
-    // Titre de la vue (défini dans le fichier vue)
+    /** Titre de la vue (défini dans le fichier vue) */
     private $titre;
-      public function __construct($action, $controleur = "") {
-   // Détermination du nom du fichier vue à partir de l'action et du constructeur
-    $fichier = "Vue/";
-    if ($controleur != "") {
-      $fichier = $fichier . $controleur . "/";
+    /**
+     * Constructeur
+     * 
+     * @param string $action Action à laquelle la vue est associée
+     * @param string $controleur Nom du contrôleur auquel la vue est associée
+     */
+    public function __construct($action, $controleur = "")
+    {
+        // Détermination du nom du fichier vue à partir de l'action et du constructeur
+        // La convention de nommage des fichiers vues est : Vue/<$controleur>/<$action>.php
+        $fichier = "Vue/";
+        if ($controleur != "") {
+            $fichier = $fichier . $controleur . "/";
+        }
+        $this->fichier = $fichier . $action . ".php";
     }
-    $this->fichier = $fichier . $action . ".php";
-  }
-    // Génère et affiche la vue
-      public function generer($donnees) {
-    // Génération de la partie spécifique de la vue
-    $contenu = $this->genererFichier($this->fichier, $donnees);
-    // On définit une variable locale accessible par la vue pour la racine Web
-    // Il s'agit du chemin vers le site sur le serveur Web
-    // Nécessaire pour les URI de type controleur/action/id
-    $racineWeb = Configuration::get("racineWeb", "/");
-    // Génération du gabarit commun utilisant la partie spécifique
-    $vue = $this->genererFichier('Vue/gabarit.php',
-      array('titre' => $this->titre, 'contenu' => $contenu,
-            'racineWeb' => $racineWeb));
-    // Renvoi de la vue générée au navigateur
-    echo $vue;
-  }
-  }
-    // Génère un fichier vue et renvoie le résultat produit
-    private function genererFichier($fichier, $donnees) {
+    /**
+     * Génère et affiche la vue
+     * 
+     * @param array $donnees Données nécessaires à la génération de la vue
+     */
+    public function generer($donnees)
+    {
+        // Génération de la partie spécifique de la vue
+        $contenu = $this->genererFichier($this->fichier, $donnees);
+        // On définit une variable locale accessible par la vue pour la racine Web
+        // Il s'agit du chemin vers le site sur le serveur Web
+        // Nécessaire pour les URI de type controleur/action/id
+        $racineWeb = Configuration::get("racineWeb", "/");
+        // Génération du gabarit commun utilisant la partie spécifique
+        $vue = $this->genererFichier('Vue/gabarit.php',
+                array('titre' => $this->titre, 'contenu' => $contenu, 'racineWeb' => $racineWeb));
+        // Renvoi de la vue générée au navigateur
+        echo $vue;
+    }
+    /**
+     * Génère un fichier vue et renvoie le résultat produit
+     * 
+     * @param string $fichier Chemin du fichier vue à générer
+     * @param array $donnees Données nécessaires à la génération de la vue
+     * @return string Résultat de la génération de la vue
+     * @throws Exception Si le fichier vue est introuvable
+     */
+    private function genererFichier($fichier, $donnees)
+    {
         if (file_exists($fichier)) {
             // Rend les éléments du tableau $donnees accessibles dans la vue
             extract($donnees);
@@ -46,9 +71,17 @@ class Vue {
             throw new Exception("Fichier '$fichier' introuvable");
         }
     }
-	
-	 // Nettoie une valeur insérée dans une page HTML
-  private function nettoyer($valeur) {
-    return htmlspecialchars($valeur, ENT_QUOTES, 'UTF-8', false);
-  }
+    /**
+     * Nettoie une valeur insérée dans une page HTML
+     * Doit être utilisée à chaque insertion de données dynamique dans une vue
+     * Permet d'éviter les problèmes d'exécution de code indésirable (XSS) dans les vues générées
+     * 
+     * @param string $valeur Valeur à nettoyer
+     * @return string Valeur nettoyée
+     */
+    private function nettoyer($valeur)
+    {
+        // Convertit les caractères spéciaux en entités HTML
+        return htmlspecialchars($valeur, ENT_QUOTES, 'UTF-8', false);
+    }
 }
