559ce84a99f1ad46e838b0456b870a6af2977dff
Melanie <melanie.xelm@gmail.com>
2019-06-24 21:02:27 +0200

    Modification index.php lien

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-
-require 'Controleur/Routeur.php';
-
+// Contrôleur frontal : instancie un routeur pour traiter la requête entrante
+require 'Framework/Routeur.php';
 $routeur = new Routeur();
 $routeur->routerRequete();
