6692ede8c4db8d2e905eade576128b84519e7f0f
Melanie <melanie.xelm@gmail.com>
2019-06-24 22:00:26 +0200

    Correction erreur Commentaire.php date

--- Modele/Commentaire.php
+++ Modele/Commentaire.php
@@ -16,8 +16,7 @@ class Commentaire extends Modele {
     // Ajoute un commentaire dans la base
   public function ajouterCommentaire($auteur, $contenu, $idBillet) {
     $sql = 'insert into T_COMMENTAIRE(COM_DATE, COM_AUTEUR, COM_CONTENU, BIL_ID)'
-      . ' values(?, ?, ?, ?)';
-    $date = date(DATE_W3C);  // Récupère la date courante
-    $this->executerRequete($sql, array($date, $auteur, $contenu, $idBillet));
+      . ' values(NOW(), ?, ?, ?)';
+    $this->executerRequete($sql, array($auteur, $contenu, $idBillet));
   }
 }
