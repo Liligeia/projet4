8f659b370778d42517650ad31a0d3f3d73ee61f8
Melanie <melanie.xelm@gmail.com>
2019-07-10 18:31:57 +0200

    Modification gabarit.php lien pour la connexion

--- Vue/gabarit.php
+++ Vue/gabarit.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
 	<base href="<?= $racineWeb ?>" >
     <link rel="stylesheet" href="Contenu/style.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
     <title><?= $titre ?></title>
   </head>
   
@@ -34,16 +35,20 @@
 		</div>
       </header>
 	  
-	<!-- Chapitres -->
+	<!-- Contenu -->
       <div id="contenu">
         <?= $contenu ?>
       </div>
 	  
 	<!-- Footer -->
       <footer id="piedBlog">
-        <div id="socialmedia">
-		</div>
+        <ul id="socialmedia">
+			<li><a class="socialicons" href= "https//www.facebook.com"><i class="icon-social-facebook"></i></a></li>
+			<li><a class="socialicons" href= "https//www.twitter.com"><i class="icon-social-twitter"></i></a></li>
+			<li><a class="socialicons" href= "https//www.instagram.com"><i class="icon-social-instagram"></i></a></li>
+		</ul>
 		<div id="connexion">
+		<a href="connexion">Connexion</a>
 		</div>
       </footer>
     </div>
