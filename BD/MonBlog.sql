b2d5cbb5fd383ce3ccd33d03eafee3cd5673fc37
Melanie <melanie.xelm@gmail.com>
2019-07-10 14:06:01 +0200

    Ajout MonBlog.sql données utilisateur

--- BD/MonBlog.sql
+++ BD/MonBlog.sql
@@ -2,6 +2,7 @@
 
 drop table if exists T_COMMENTAIRE cascade;
 drop table if exists T_BILLET cascade;
+drop table if exists T_UTILISATEUR cascade;
 
 create table T_BILLET (
   BIL_ID integer primary key auto_increment,
@@ -19,6 +20,13 @@ create table T_COMMENTAIRE (
   constraint fk_com_bil foreign key(BIL_ID) references T_BILLET(BIL_ID)
 ) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+create table T_UTILISATEUR (
+  UTIL_ID integer primary key auto_increment,
+  UTIL_LOGIN varchar(100) not null,
+  UTIL_MDP varchar(100) not null
+) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
 insert into T_BILLET(BIL_DATE, BIL_TITRE, BIL_CONTENU) values
 (NOW(), 'Premier billet', 'Bonjour monde ! Ceci est le premier billet sur mon blog.');
 insert into T_BILLET(BIL_DATE, BIL_TITRE, BIL_CONTENU) values
@@ -28,3 +36,6 @@ insert into T_COMMENTAIRE(COM_DATE, COM_AUTEUR, COM_CONTENU, BIL_ID) values
 (NOW(), 'A. Nonyme', 'Bravo pour ce début', 1);
 insert into T_COMMENTAIRE(COM_DATE, COM_AUTEUR, COM_CONTENU, BIL_ID) values
 (NOW(), 'Moi', 'Merci ! Je vais continuer sur ma lancée', 1);
+
+insert into T_UTILISATEUR(UTIL_LOGIN, UTIL_MDP) values
+('admin', 'secret');
