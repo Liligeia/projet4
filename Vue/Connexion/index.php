e54971b009706c09fdd0814705d70f820faac1f6
Melanie <melanie.xelm@gmail.com>
2019-07-10 18:36:16 +0200

    Modification index.php (connexion) id

--- Vue/Connexion/index.php
+++ Vue/Connexion/index.php
@@ -2,10 +2,10 @@
 
 <p>Vous devez être connecté pour accéder à cette zone.</p>
 
-<form action="connexion/connecter" method="post">
+<form id="formConnect" action="connexion/connecter" method="post">
     <input name="login" type="text" placeholder="Entrez votre login" required autofocus>
     <input name="mdp" type="password" placeholder="Entrez votre mot de passe" required>
-    <button type="submit">Connexion</button>
+    <button id="submitConnect" type="submit">Connexion</button>
 </form>
 
 <?php if (isset($msgErreur)): ?>
