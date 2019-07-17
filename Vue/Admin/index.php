9615efb3299ac3e939e99e5f91c2594fa83c9b9f
Melanie <melanie.xelm@gmail.com>
2019-07-17 17:26:59 +0200

    Ajout index.php (Admin) textarea tinyMCE

--- Vue/Admin/index.php
+++ Vue/Admin/index.php
@@ -5,4 +5,7 @@
 Bienvenue, <?= $this->nettoyer($login) ?> !
 Ce blog comporte <?= $this->nettoyer($nbBillets) ?> billet(s) et <?= $this->nettoyer($nbCommentaires) ?> commentaire(s).
 <br>
+
+<textarea id="mytextarea">Hello, World!</textarea>
+
 <a id="lienDeco" href="connexion/deconnecter">Se déconnecter</a>
