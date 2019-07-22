247f1971db605b8b6855874703df88fbf14ac63d
Melanie <melanie.xelm@gmail.com>
2019-07-22 15:54:36 +0200

    Ajout index.php (Admin) Création d'un nouveau chapitre

--- Vue/Admin/index.php
+++ Vue/Admin/index.php
@@ -1,4 +1,4 @@
-<?php $this->titre = "Mon Blog - Administration" ?>
+<?php $this->titre = "Administration" ?>
 
 <h2>Administration</h2>
 
@@ -6,8 +6,18 @@ Bienvenue, <?= $this->nettoyer($login) ?> !
 Ce blog comporte <?= $this->nettoyer($nbBillets) ?> billet(s) et <?= $this->nettoyer($nbCommentaires) ?> commentaire(s).
 <br>
 
-  <form method="post">
-    <textarea id="mytextarea">Hello, World!</textarea>
+
+<h4> Créer un nouveau chapitre :</h4>
+
+  <form id="formChap" method="post">
+	<input type="text" id="titleChapter" name="chapitre" placeholder="Titre du chapitre"/>
+    <textarea id="mytextarea"></textarea>
+	<input type="submit" id="submitChap" value="Publier"/>
   </form>
+  
+<h4>Modifier un chapitre :</h4>
+
+<h4>"NOMBRE" commentaire(s) signalé(s) :</h4>  
+
 
 <a id="lienDeco" href="connexion/deconnecter">Se déconnecter</a>
