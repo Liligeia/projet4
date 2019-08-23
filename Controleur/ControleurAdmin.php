17183718bbf4c4dbcb617e3df9e2f2ecbfd0d02e
Melanie <melanie.xelm@gmail.com>
2019-08-23 17:16:42 +0200

    Ajout ControleurAdmin.php fonction publier

--- Controleur/ControleurAdmin.php
+++ Controleur/ControleurAdmin.php
@@ -24,4 +24,19 @@ class ControleurAdmin extends ControleurSecurise
         $login = $this->requete->getSession()->getAttribut("login");
         $this->genererVue(array('nbBillets' => $nbBillets, 'nbCommentaires' => $nbCommentaires, 'login' => $login));
     }
+	
+	//Ajoute un chapitre dans la liste des billets
+	public function publier() 
+	{
+	  $titre = $this->requete->getParametre("titre");
+	  $contenu = $this->requete->getParametre("contenu");
+	  
+	  $this->billet->ajouterBillet($titre, $contenu);
+	  
+	  // Exécution de l'action par défaut pour actualiser la liste des billets
+	  $this->executerAction("index");
+    }	
+	
+	
+	
 }
