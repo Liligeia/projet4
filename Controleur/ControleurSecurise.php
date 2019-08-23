22827412eb1c66a36f02a9128337167153b4b0da
Melanie <melanie.xelm@gmail.com>
2019-08-23 17:58:54 +0200

    Modification ControleurSecurise.php attribut login

--- Controleur/ControleurSecurise.php
+++ Controleur/ControleurSecurise.php
@@ -10,7 +10,7 @@ abstract class ControleurSecurise extends Controleur
         // Vérifie si les informations utilisateur sont présents dans la session
         // Si oui, l'utilisateur s'est déjà authentifié : l'exécution de l'action continue normalement
         // Si non, l'utilisateur est renvoyé vers le contrôleur de connexion
-        if ($this->requete->getSession()->existeAttribut("idUtilisateur")) {
+        if ($this->requete->getSession()->existeAttribut("login")) {
             parent::executerAction($action);
         }
         else {
