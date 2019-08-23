a892a09256734696951b82777214e9649a51ff1a
Melanie <melanie.xelm@gmail.com>
2019-08-23 17:44:36 +0200

    Modification index.php(Admin) name=titre

--- Vue/Admin/index.php
+++ Vue/Admin/index.php
@@ -10,7 +10,7 @@ Ce blog comporte <?= $this->nettoyer($nbBillets) ?> billet(s) et <?= $this->nett
 <h4> Créer un nouveau chapitre :</h4>
 
   <form id="formChap" method="post" action="admin/publier>
-	<input type="text" id="titleChapter" name="chapitre" placeholder="Titre du chapitre"/>
+	<input type="text" id="titleChapter" name="titre" placeholder="Titre du chapitre"/>
     <textarea id="mytextarea"></textarea>
 	<input type="submit" id="submitChap" value="Publier"/>
   </form>
