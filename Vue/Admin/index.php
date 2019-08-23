650a07a98334e267677b81f1b2ff986e0d8eb63f
Melanie <melanie.xelm@gmail.com>
2019-08-23 18:04:43 +0200

    Correction index.php(Admin) guillements l12

--- Vue/Admin/index.php
+++ Vue/Admin/index.php
@@ -9,7 +9,7 @@ Ce blog comporte <?= $this->nettoyer($nbBillets) ?> billet(s) et <?= $this->nett
 
 <h4> Créer un nouveau chapitre :</h4>
 
-  <form id="formChap" method="post" action="admin/publier>
+  <form id="formChap" method="post" action="admin/publier">
 	<input type="text" id="titleChapter" name="titre" placeholder="Titre du chapitre"/>
     <textarea id="mytextarea" name="contenu"></textarea>
 	<input type="submit" id="submitChap" value="Publier"/>
