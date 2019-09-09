aa123c24fd9859422478633ee2cee835dfdd7646
Melanie <melanie.xelm@gmail.com>
2019-09-09 18:22:39 +0200

    Création index.php(Editbillets)

---
+++ Vue/Editbillets/index.php
@@ -0,0 +1,9 @@
+<?php $this->titre = "Modifier billet"; ?>
+
+<h4> Modifier votre chapitre :</h4>
+
+  <form id="formChap" method="post" action="">
+	<input type="text" id="titleChapter" name="titre" placeholder="Titre du chapitre"/>
+    <textarea id="mytextarea" name="contenu"></textarea>
+	<input type="submit" class="buttonsub" value="Modifier"/>
+  </form>
