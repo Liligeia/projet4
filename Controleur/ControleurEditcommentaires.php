37ce58872904fd34f901035f9074d0edd947f9f3
Melanie <melanie.xelm@gmail.com>
2019-09-09 16:26:31 +0200

    Création ControleurEditcommentaires.php

---
+++ Controleur/ControleurEditcommentaires.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'ControleurSecurise.php';
+require_once 'Modele/Commentaire.php';
+/**
+ * Contrôleur des actions d'administration
+ */
+class ControleurEditcommentaires extends ControleurSecurise
+{
+	
+	  private $commentaire;
+
+  public function __construct() {
+    $this->commentaire = new Commentaire();
+  }
+
+  // Affiche la liste de tous les commentaires du blog
+  public function index() {
+    $commentaires = $this->commentaire->getListCommentaires();
+    $this->genererVue(array('commentaires' => $commentaires));
+  }
+	
+}
