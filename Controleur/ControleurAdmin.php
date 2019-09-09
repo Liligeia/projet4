36ab09f97b02dced39e2af337d18ac8901289245
Melanie <melanie.xelm@gmail.com>
2019-09-09 16:17:08 +0200

    Modification ControleurAdmin.php liste chapitre

--- Controleur/ControleurAdmin.php
+++ Controleur/ControleurAdmin.php
@@ -1,42 +1,31 @@
 <?php
 require_once 'ControleurSecurise.php';
 require_once 'Modele/Billet.php';
-require_once 'Modele/Commentaire.php';
 /**
  * Contrôleur des actions d'administration
  */
 class ControleurAdmin extends ControleurSecurise
 {
-    private $billet;
-    private $commentaire;
-    /**
-     * Constructeur 
-     */
-    public function __construct()
-    {
-        $this->billet = new Billet();
-        $this->commentaire = new Commentaire();
-    }
-    public function index()
-    {
-        $nbBillets = $this->billet->getNombreBillets();
-        $nbCommentaires = $this->commentaire->getNombreCommentaires();
-        $login = $this->requete->getSession()->getAttribut("login");
-        $this->genererVue(array('nbBillets' => $nbBillets, 'nbCommentaires' => $nbCommentaires, 'login' => $login));
-    }
 	
-	//Ajoute un chapitre dans la liste des billets
-	public function publier() 
-	{
-	  $titre = $this->requete->getParametre("titre");
-	  $contenu = $this->requete->getParametre("contenu");
+	  private $billet;
+
+  public function __construct() {
+    $this->billet = new Billet();
+  }
+
+  // Affiche la liste de tous les billets du blog
+  public function index() {
+    $billets = $this->billet->getBillets();
+    $this->genererVue(array('billets' => $billets));
+  }
+  
+  public function supprimer(){
+	$idBillet = $this->requete->getParametre("id");
 	  
-	  $this->billet->ajouterBillet($titre, $contenu);
+	$this->billet->supprimerBillet($idBillet);
 	  
-	  // Exécution de l'action par défaut pour actualiser la liste des billets
-	  $this->executerAction("index");
-    }	
-	
+	$this->executerAction("index");
+  }
 	
 	
 }
