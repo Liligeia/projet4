e1a86a3022b19195129b208c8f4845537295910a
Melanie <melanie.xelm@gmail.com>
2019-09-23 19:19:10 +0200

    Déplacement Billet.php com en cascade

--- Modele/Billet.php
+++ Modele/Billet.php
@@ -51,7 +51,76 @@ class Billet extends Modele {
 		$sql = 'delete from T_COMMENTAIRE where BIL_ID=?';
 		$this->executerRequete($sql, array($idBillet));
 	}
+	<?php
+require_once 'Framework/Modele.php';
+/**
+ * Fournit les services d'accès aux genres musicaux 
+ * 
+ */
+class Billet extends Modele {
+    /** Renvoie la liste des billets du blog
+     * 
+     * @return PDOStatement La liste des billets
+     */
+    public function getBillets() {
+        $sql = 'select BIL_ID as id, BIL_DATE as date,'
+                . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+                . ' order by BIL_ID desc';
+        $billets = $this->executerRequete($sql);
+        return $billets;
+    }
+    /** Renvoie les informations sur un billet
+     * 
+     * @param int $id L'identifiant du billet
+     * @return array Le billet
+     * @throws Exception Si l'identifiant du billet est inconnu
+     */
+    public function getBillet($idBillet) {
+        $sql = 'select BIL_ID as id, BIL_DATE as date,'
+                . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+                . ' where BIL_ID=?';
+        $billet = $this->executerRequete($sql, array($idBillet));
+        if ($billet->rowCount() > 0)
+            return $billet->fetch();  // Accès à la première ligne de résultat
+        else
+            throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
+    }
 	
+	/* Ajoute un billet dans la BD*/
+	public function ajouterBillet($titre, $contenu) {
+		$sql = 'insert into T_BILLET (BIL_DATE, BIL_TITRE, BIL_CONTENU)'
+			.'values(?, ?, ?)';
+		$date = date('Y-m-d H:i:s');
+		$this->executerRequete($sql, array($date, $titre, $contenu));
+		
+	}
+	/*Supprime un billet dans la BD*/
+	public function supprimerBillet($idBillet){
+		$sql = 'delete from T_BILLET where BIL_ID=?';
+		$this->executerRequete($sql, array($idBillet));
+	}
+	
+	/*Update un billet dans la BD*/
+	public function updateBillet($titre, $contenu, $idBillet){
+		$sql = ' update T_BILLET'
+				.' set BIL_TITRE=?, BIL_CONTENU=?'
+				.' where BIL_ID=?';
+		$this->executerRequete($sql, array($titre, $contenu, $idBillet));		
+	}
+	
+    /**
+     * Renvoie le nombre total de billets
+     * 
+     * @return int Le nombre de billets
+     */
+    public function getNombreBillets()
+    {
+        $sql = 'select count(*) as nbBillets from T_BILLET';
+        $resultat = $this->executerRequete($sql);
+        $ligne = $resultat->fetch();  // Le résultat comporte toujours 1 ligne
+        return $ligne['nbBillets'];
+    }
+}
 	/*Update un billet dans la BD*/
 	public function updateBillet($titre, $contenu, $idBillet){
 		$sql = ' update T_BILLET'
