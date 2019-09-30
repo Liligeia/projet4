1b4a99328322458fef23ae29330578bba8ba653b
Melanie <melanie.xelm@gmail.com>
2019-09-30 14:44:31 +0200

    Ajout index.php(newbillet) class confirm

--- Vue/Newbillet/index.php
+++ Vue/Newbillet/index.php
@@ -3,8 +3,8 @@
 
 <h4> Créer un nouveau chapitre :</h4>
 
-  <form id="formChap" method="post" action="newbillet/publier">
+  <form id="formChap" name="formConf" method="post" action="newbillet/publier">
 	<input type="text" id="titleChapter" name="titre" placeholder="Titre du chapitre"/>
     <textarea id="mytextarea" name="contenu"></textarea>
-	<input type="submit" class="buttonsub" value="Publier"/>
+	<input type="button" class="buttonsub confirm" value="Publier"/>
   </form>
