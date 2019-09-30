06a88ff4dd01c5fa463c0e114f3161fbc9f4d64d
Melanie <melanie.xelm@gmail.com>
2019-09-30 14:45:45 +0200

    Ajout index.php(editbillets) form

--- Vue/Editbillets/index.php
+++ Vue/Editbillets/index.php
@@ -2,9 +2,9 @@
 
 <h4> Modifier votre chapitre :</h4>
 
-  <form id="formChap" method="post" action="editbillets/update">
+  <form id="formChap" method="post" name="formConf" action="editbillets/update">
 	<input type="text" id="titleChapter" name="titre" placeholder="Titre du chapitre" value="<?= $this->nettoyer($billet['titre']) ?>"/>
     <textarea id="mytextarea" name="contenu"><?= $this->nettoyer($billet['contenu']) ?></textarea>
 	<input type="hidden" name="id" value="<?= $billet['id'] ?>" />
-	<input type="submit" class="buttonsub" value="Modifier"/>
+	<input type="button" id="sub" class="buttonsub confirm" value="Modifier"/>
   </form>
