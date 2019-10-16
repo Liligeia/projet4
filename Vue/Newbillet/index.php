ade3c76f008b6d082ccbd651f5ff38e47b9901bd
Melanie <melanie.xelm@gmail.com>
2019-10-16 17:30:40 +0200

    Ajout index.php(newbillet) bouton annuler

--- Vue/Newbillet/index.php
+++ Vue/Newbillet/index.php
@@ -1,5 +1,6 @@
 <?php $this->titre = "Nouveau Billet"; ?>
 
+<h2>Administration</h2>
 
 <h4> Créer un nouveau chapitre :</h4>
 
@@ -7,4 +8,5 @@
 	<input type="text" id="titleChapter" name="titre" placeholder="Titre du chapitre"/>
     <textarea id="mytextarea" name="contenu"></textarea>
 	<input type="button" class="buttonsub confirm" value="Publier"/>
+	<input type="button" class="buttonsub" value="Annuler" onclick="history.back()"  />
   </form>
